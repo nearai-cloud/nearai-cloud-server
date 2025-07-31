ad725f0dc902ea55775d8706406269852908d50a
Coffee <zjchen1234@foxmail.com>
2025-07-31 20:03:04 +0800

    feat: init

--- src/server/routes/user/registerUser.ts
+++ src/server/routes/user/registerUser.ts
@@ -1,8 +1,12 @@
 import { RequestHandler } from 'express';
 import ctx from 'express-http-context';
+import * as v from 'valibot';
 import { lightLLM } from '../../../services/light-llm';
 import { CONTEXT_KEYS } from '../../../utils/consts';
 import { WeakAuth, weakAuth } from '../../middlewares/auth';
+import { parseOutput } from '../../../utils/parsers';
+
+const outputSchema = v.object({});
 
 const resolver: RequestHandler = async (req, res) => {
   const { authUser }: WeakAuth = ctx.get(CONTEXT_KEYS.WEAK_AUTH);
@@ -12,7 +16,9 @@ const resolver: RequestHandler = async (req, res) => {
     userEmail: authUser.email,
   });
 
-  res.json({});
+  const output = parseOutput(outputSchema, {});
+
+  res.json(output);
 };
 
 export const registerUser: RequestHandler[] = [weakAuth, resolver];
