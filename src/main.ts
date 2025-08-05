0e0cfb1813e03ccce426f3369dd07438353723f3
Coffee <zjchen1234@foxmail.com>
2025-08-05 21:07:34 +0800

    refactor

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import { runServer } from './server/app';
 import { sendSlackInfo } from './services/slack';
-import { config } from './config';
 
 async function main() {
-  console.log(config);
-
   runServer();
   await sendSlackInfo('Server started');
 }
