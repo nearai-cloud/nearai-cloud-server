b0a43b8a38cb414184a4573cbcd2d55bab1c86a4
Coffee <zjchen1234@foxmail.com>
2025-08-05 15:54:23 +0800

    chore

--- src/server/routes/api/chat-completions.ts
+++ src/server/routes/api/chat-completions.ts
@@ -7,9 +7,7 @@ export const chatCompletions: RequestHandler[] = [
   keyAuthMiddleware,
   async (req, res) => {
     const { litellmClient }: KeyAuth = ctx.get(CTX_GLOBAL_KEYS.KEY_AUTH);
-
     const data = await litellmClient.chatCompletions(req.body);
-
     if ('pipe' in data) {
       data.pipe(res);
     } else {
