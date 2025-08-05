4e0f818ba43eea3d7533d11987a2b42e37bc6d88
Coffee <zjchen1234@foxmail.com>
2025-08-05 16:14:13 +0800

    fix: catch error

--- src/server/routes/openai/chat-completions.ts
+++ src/server/routes/openai/chat-completions.ts
@@ -2,16 +2,33 @@ import ctx from 'express-http-context';
 import { RequestHandler } from 'express';
 import { KeyAuth, keyAuthMiddleware } from '../../middlewares/auth';
 import { CTX_GLOBAL_KEYS } from '../../../utils/consts';
+import { throwHttpError } from '../../../utils/error';
+import { LitellmClientError } from '../../../services/litellm';
 
 export const chatCompletions: RequestHandler[] = [
   keyAuthMiddleware,
   async (req, res) => {
     const { litellmClient }: KeyAuth = ctx.get(CTX_GLOBAL_KEYS.KEY_AUTH);
-    const data = await litellmClient.chatCompletions(req.body);
-    if ('pipe' in data) {
-      data.pipe(res);
+
+    let completions;
+
+    try {
+      completions = await litellmClient.chatCompletions(req.body);
+    } catch (e: unknown) {
+      if (e instanceof LitellmClientError) {
+        throwHttpError({
+          status: Number(e.code),
+          cause: e,
+        });
+      }
+
+      throw e;
+    }
+
+    if ('pipe' in completions) {
+      completions.pipe(res);
     } else {
-      res.json(data);
+      res.json(completions);
     }
   },
 ];
--- src/server/routes/openai/models.ts
+++ src/server/routes/openai/models.ts
@@ -2,12 +2,29 @@ import ctx from 'express-http-context';
 import { RequestHandler } from 'express';
 import { KeyAuth, keyAuthMiddleware } from '../../middlewares/auth';
 import { CTX_GLOBAL_KEYS } from '../../../utils/consts';
+import { LitellmClientError } from '../../../services/litellm';
+import { throwHttpError } from '../../../utils/error';
 
 export const models: RequestHandler[] = [
   keyAuthMiddleware,
   async (req, res) => {
     const { litellmClient }: KeyAuth = ctx.get(CTX_GLOBAL_KEYS.KEY_AUTH);
-    const models = await litellmClient.models();
+
+    let models;
+
+    try {
+      models = await litellmClient.models();
+    } catch (e: unknown) {
+      if (e instanceof LitellmClientError) {
+        throwHttpError({
+          status: Number(e.code),
+          cause: e,
+        });
+      }
+
+      throw e;
+    }
+
     res.json(models);
   },
 ];
