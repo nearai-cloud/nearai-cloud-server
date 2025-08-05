da08c1cd326cea48d9cd2e0a7bc644aa6917e37d
Coffee <zjchen1234@foxmail.com>
2025-08-05 22:25:49 +0800

    fix: open ai interface path

--- src/server/routes/index.ts
+++ src/server/routes/index.ts
@@ -1,16 +1,17 @@
 import express from 'express';
 import { notFoundMiddleware } from '../middlewares/not-found';
+import { openaiRouter } from './openai';
 import { ping } from './ping';
 import { userRouter } from './user';
 import { keyRouter } from './key';
-import { openaiRouter } from './openai';
 
 // TODO: Need Fix: Currently 405 Method Not Allowed will be considered as 404 Not Found
 export const router = express.Router();
 
+router.use('', openaiRouter);
+
 router.get('/', ping);
 router.use('/user', userRouter);
 router.use('/key', keyRouter);
-router.use('/openai', openaiRouter);
 
 router.use(notFoundMiddleware);
