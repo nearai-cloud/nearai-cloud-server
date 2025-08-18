2cdbcc50ddd84b7f7c52d6c6ef675977edd9dd47
Coffee <zjchen1234@foxmail.com>
2025-08-18 17:36:11 +0800

    fix: check model name

--- src/server/routes/model/create-model.ts
+++ src/server/routes/model/create-model.ts
@@ -4,7 +4,7 @@ import { litellmServiceAccountAuthMiddleware } from '../../middlewares/auth';
 import { adminLitellmApiClient } from '../../../services/litellm-api-client';
 
 const inputSchema = v.object({
-  model: v.string(),
+  model: v.pipe(v.string(), v.regex(/[a-zA-Z0-9_-]+/)),
   providerModelName: v.string(),
   providerName: v.string(),
   credentialName: v.string(),
