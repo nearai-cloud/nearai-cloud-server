717fd4e1fdaf2d2409c52a0feee189469544114b
Coffee <zjchen1234@foxmail.com>
2025-08-27 21:22:05 +0800

    feat: test

---
+++ test/mock.ts
@@ -0,0 +1,66 @@
+import { AuthError, User } from "@supabase/supabase-js";
+
+export const mockUserRecord = {
+  alice: {
+    id: 'alice',
+    email: 'alice@near.org',
+    supabaseAuthToken: 'Bearer alice',
+  },
+};
+
+export const mockUserList = Object.values(mockUserRecord);
+
+type MockUserResponse = {
+  data: {
+    user: Partial<User>; // We just use partial fields
+  },
+  error: null
+} | {
+  data: {
+    user: null;
+  },
+  error: AuthError;
+}
+
+jest.mock('@supabase/supabase-js', () => {
+  const originalModule = jest.requireActual('@supabase/supabase-js');
+
+  return {
+    ...originalModule,
+    createClient: jest.fn(() => ({
+      auth: {
+        getUser: jest.fn(async (jwt?: string): Promise<MockUserResponse> => {
+          if (!jwt) {
+            return {
+              data: {
+                user: null
+              },
+              error: new AuthError('Invalid authorization', 401),
+            }
+          }
+
+          const user = mockUserList.find(user => user.supabaseAuthToken === jwt)
+
+          if (!user) {
+            return {
+              data: {
+                user: null
+              },
+              error: new AuthError('Invalid authorization', 401),
+            }
+          }
+
+          return {
+            data: {
+              user: {
+                id: user.id,
+                email: user.email,
+              }
+            },
+            error: null,
+          }
+        }),
+      },
+    })),
+  };
+});
