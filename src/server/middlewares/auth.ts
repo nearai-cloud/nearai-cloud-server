0d538351ea19cecfbec86f92a551dbabec76b9fa
Coffee <zjchen1234@foxmail.com>
2025-08-29 18:50:53 +0800

    fix: security

--- src/server/middlewares/auth.ts
+++ src/server/middlewares/auth.ts
@@ -4,6 +4,7 @@ import ctx from 'express-http-context';
 import {
   BEARER_TOKEN_PREFIX,
   CTX_GLOBAL_KEYS,
+  LITELLM_KEY_PREFIX,
   STATUS_CODES,
 } from '../../utils/consts';
 import { createSupabaseClient } from '../../services/supabase';
@@ -147,6 +148,13 @@ async function authorizeKey(authorization?: string): Promise<KeyAuth> {
 
   const token = authorization.slice(BEARER_TOKEN_PREFIX.length);
 
+  if (!token.startsWith(LITELLM_KEY_PREFIX)) {
+    throw createOpenAiHttpError({
+      status: STATUS_CODES.UNAUTHORIZED,
+      message: 'Invalid authorization token',
+    });
+  }
+
   let key: Key | null;
 
   try {
@@ -188,6 +196,13 @@ export async function authorizeLitellmServiceAccount(authorization?: string) {
   }
 
   const token = authorization.slice(BEARER_TOKEN_PREFIX.length);
+
+  if (!token.startsWith(LITELLM_KEY_PREFIX)) {
+    throw createOpenAiHttpError({
+      status: STATUS_CODES.UNAUTHORIZED,
+      message: 'Invalid authorization token',
+    });
+  }
 
   const key = await adminLitellmApiClient.getKey({
     keyOrKeyHash: token,
