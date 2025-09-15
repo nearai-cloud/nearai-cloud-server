55f01f3c14782c5e616643606e1fad2958cb2fba
Coffee <zjchen1234@foxmail.com>
2025-09-15 10:37:05 +0800

    fix

--- src/server/middlewares/body-parser.ts
+++ src/server/middlewares/body-parser.ts
@@ -1,7 +1,7 @@
 import express, { RequestHandler } from 'express';
 
 export const bodyParserMiddleware: RequestHandler = (req, res, next) => {
-  if (req.path.includes('/chat/completions')) {
+  if (req.path.includes('/completions')) {
     express.raw({ type: '*/*' })(req, res, next);
   } else {
     express.json()(req, res, next);
