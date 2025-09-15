31b9ce65abd1671c41e4ac7deef3c20baee02053
Coffee <zjchen1234@foxmail.com>
2025-09-15 10:36:17 +0800

    feat: support x-request-hash

--- src/server/routes/openai/completions.ts
+++ src/server/routes/openai/completions.ts
@@ -1,12 +1,24 @@
 import { BEARER_TOKEN_PREFIX } from '../../../utils/consts';
 import { createRouteResolver } from '../../middlewares/route-resolver';
 import { createLitellmApiClient } from '../../../services/litellm-api-client';
+import { createHash } from 'crypto';
 
 export const completions = createRouteResolver({
   resolve: async ({ req }) => {
     const litellmApiClient = createLitellmApiClient(
       req.headers.authorization?.slice(BEARER_TOKEN_PREFIX.length) ?? '',
     );
-    return litellmApiClient.completions(req.body);
+
+    const xRequestHash = createHash('sha256')
+      .update(req.body)
+      .digest()
+      .toString('hex');
+
+    return litellmApiClient.completions({
+      ...JSON.parse(req.body.toString()),
+      extra_headers: {
+        'X-Request-Hash': xRequestHash,
+      },
+    });
   },
 });
